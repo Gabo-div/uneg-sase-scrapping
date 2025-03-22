3db5117de369d4efbbac98b950861c744747764e
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2025-03-22 10:39:30 -0400

    refactor: api lib in status action

--- src/actions/status.ts
+++ src/actions/status.ts
@@ -1,16 +1,11 @@
 "use server";
 
-import ky from "ky";
+import { api } from "@/lib/api";
 
 export const checkStatus = async () => {
-  return await ky
+  return await api
     .get("https://servicio.uneg.edu.ve/sase/principal/login.php", {
       retry: 0,
-      headers: {
-        Origin: "https://servicio.uneg.edu.ve/",
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.8177.1037 Safari/537.36",
-      },
     })
     .text();
 };
